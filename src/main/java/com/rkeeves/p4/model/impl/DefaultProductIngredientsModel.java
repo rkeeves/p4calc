d6201d3461cbad67d2fd194c16b299211110265f
rkeeves <aprettydecentaddress@gmail.com>
2021-05-04 15:39:57 +0200

    Implemented product ingredients model

---
+++ src/main/java/com/rkeeves/p4/model/impl/DefaultProductIngredientsModel.java
@@ -0,0 +1,37 @@
+package com.rkeeves.p4.model.impl;
+
+import com.rkeeves.p4.model.ProductBasicPropertiesModel;
+import com.rkeeves.p4.model.ProductIngredientsModel;
+import javafx.beans.property.DoubleProperty;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Data
+@Builder(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class DefaultProductIngredientsModel implements ProductIngredientsModel {
+
+    private final Map<ProductBasicPropertiesModel, DoubleProperty> ingredients;
+
+    public static DefaultProductIngredientsModel create(){
+        return DefaultProductIngredientsModel.builder()
+                .ingredients(new HashMap<>())
+                .build();
+    }
+
+    @Override
+    public Optional<DoubleProperty> getIngredientQuantity(String ingredientName) {
+        for (var ingredientAndQuantityPair: ingredients.entrySet()) {
+            if(ingredientAndQuantityPair.getKey().getNameProperty().get().equals(ingredientName)){
+                return Optional.of(ingredientAndQuantityPair.getValue());
+            }
+        }
+        return Optional.empty();
+    }
+}
