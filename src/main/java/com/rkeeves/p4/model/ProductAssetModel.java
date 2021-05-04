12c443bed30233ddb7c43193026893c2097b5110
rkeeves <aprettydecentaddress@gmail.com>
2021-05-04 15:33:51 +0200

    Added interface for product asset related props

---
+++ src/main/java/com/rkeeves/p4/model/ProductAssetModel.java
@@ -0,0 +1,26 @@
+package com.rkeeves.p4.model;
+
+import javafx.beans.binding.NumberExpression;
+
+/**
+ * This entity provides access to all derived properties related to owned assets for a given product.
+ */
+public interface ProductAssetModel {
+
+    /**
+     * The required number of required workshops to fulfill the market demand after all multipliers are applied.
+     * This number represents the gross number of workshops.
+     * Aka not just the number of workshops which are needed to produce the market demand, but also to fulfill demand
+     * coming from other workshops if this product is an ingredient of an other needed good.
+     *
+     * @return number of required workshops
+     */
+    NumberExpression getWorkshopCountProperty();
+
+    /**
+     * The required number of workers to fully staff all required workshops (for a given product).
+     *
+     * @return the number of workers
+     */
+    NumberExpression getRequiredWorkerCountProperty();
+}
