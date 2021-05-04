5a9bcb35e3799a59c75f776b6866924d006f6f47
rkeeves <aprettydecentaddress@gmail.com>
2021-05-04 15:33:22 +0200

    Added interface for product demand related props

---
+++ src/main/java/com/rkeeves/p4/model/ProductDemandModel.java
@@ -0,0 +1,42 @@
+package com.rkeeves.p4.model;
+
+import javafx.beans.binding.NumberExpression;
+
+/**
+ * This entity provides access to all derived properties related to demand for a given product.
+ */
+public interface ProductDemandModel {
+
+    /**
+     * The base demand in barrels for a given product.
+     * This represents demand ONLY by consumers, NOT by industries using this product as ingredient.
+     *
+     * @return base demand
+     */
+    NumberExpression getBaseDemandProperty();
+
+    /**
+     * The demand after every multiplier and modifier is applied to base demand (for a given product).
+     * This represents demand ONLY by consumers, NOT by industries using this product as ingredient.
+     *
+     * @return market demand
+     */
+    NumberExpression getMarketDemandProperty();
+
+    /**
+     * This represents demand by consumers AND by industries using this product as ingredient.
+     *
+     * @return sum of market demand and industry demand
+     */
+    NumberExpression getSumDemandProperty();
+
+    /**
+     * Binds the sum demand to a given expression.
+     * Due to dependency issues the sum must be set after all product objects are constructed.
+     * The products are not independent of each other
+     * because a workshop of one product type can use other products as ingredients.
+     *
+     * @param sumDemandExpression
+     */
+    void bindSumDemandExpression(NumberExpression sumDemandExpression);
+}
