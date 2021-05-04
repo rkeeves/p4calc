fdc3f77704e13567da025ede31c2d7a6564ce506
rkeeves <aprettydecentaddress@gmail.com>
2021-05-04 15:30:45 +0200

    Added mapper for Product dto

---
+++ src/main/java/com/rkeeves/p4/dto/ProductDTOMapper.java
@@ -0,0 +1,27 @@
+package com.rkeeves.p4.dto;
+
+import com.rkeeves.p4.model.ProductModel;
+
+import java.util.HashMap;
+
+public class ProductDTOMapper implements DTOMapper<ProductModel, ProductDTO> {
+
+    @Override
+    public ProductDTO toDTO(ProductModel model) {
+        var dto = new ProductDTO();
+        dto.setName(model.getNameProperty().get());
+        dto.setMarketDemandFulfillmentRatio(model.getMarketDemandFulfillmentRatioProperty().get());
+        dto.setBaseDemandInKg(model.getBaseDemandInKgProperty().get());
+        dto.setProductionPerWorkshop(model.getProductionPerWorkshopProperty().get());
+        dto.setBasePrice(model.getBasePriceProperty().get());
+        var ingredients = model.getIngredients();
+        var dtoIngredients = new HashMap<String,Double>();
+        for (var ingredientModelAndWeightPair: ingredients.entrySet()) {
+            var name = ingredientModelAndWeightPair.getKey().getNameProperty().get();
+            var weight = ingredientModelAndWeightPair.getValue().get();
+            dtoIngredients.put(name,weight);
+        }
+        dto.setIngredients(dtoIngredients);
+        return dto;
+    }
+}
