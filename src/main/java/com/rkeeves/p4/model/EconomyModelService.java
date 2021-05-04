8b8e1df4d1b5a6e8a780e02964dcc8400084486a
rkeeves <aprettydecentaddress@gmail.com>
2021-05-04 15:37:33 +0200

    Added service (entry point to Economy API)

---
+++ src/main/java/com/rkeeves/p4/model/EconomyModelService.java
@@ -0,0 +1,17 @@
+package com.rkeeves.p4.model;
+
+import com.rkeeves.p4.dto.EconomyDTO;
+
+/**
+ * This service is responsible for providing EconomyModel for the user.
+ */
+public interface EconomyModelService {
+
+    /**
+     * Based on the economy model dto given by a user, provides a Property-based model of the economy.
+     *
+     * @param dto - the Data Transfer Object representing the economy
+     * @return model of the economy
+     */
+    EconomyModel create(EconomyDTO dto);
+}
