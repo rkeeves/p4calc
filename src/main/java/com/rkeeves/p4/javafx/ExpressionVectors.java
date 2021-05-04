28ff17c478ae576728a0298232e8367942b13eda
rkeeves <aprettydecentaddress@gmail.com>
2021-05-04 15:20:54 +0200

    Added utility class for ExpressionVector

---
+++ src/main/java/com/rkeeves/p4/javafx/ExpressionVectors.java
@@ -0,0 +1,39 @@
+package com.rkeeves.p4.javafx;
+
+import javafx.beans.binding.DoubleExpression;
+import javafx.beans.binding.NumberExpression;
+import javafx.beans.property.SimpleDoubleProperty;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ExpressionVectors {
+
+    private ExpressionVectors(){
+
+    }
+
+    public static ArrayExpressionVector arrayExpressionVector(double[] defaultValues){
+        NumberExpression[] array = new DoubleExpression[defaultValues.length];
+        for (int i = 0; i < defaultValues.length; i++) {
+            array[i] = new SimpleDoubleProperty(defaultValues[i]);
+        }
+        return new ArrayExpressionVector(array);
+    }
+
+    public static double[] toArray(ExpressionVector vector){
+        double[] array = new double[vector.size()];
+        for (int i = 0; i < vector.size(); i++) {
+            array[i] = vector.get(i).doubleValue();
+        }
+        return array;
+    }
+
+    public static String toString(ExpressionVector vector){
+        return IntStream.range(0,vector.size())
+                .mapToObj(vector::get)
+                .map(NumberExpression::doubleValue)
+                .map(Numbers::doubleToString)
+                .collect(Collectors.joining(", "));
+    }
+}
