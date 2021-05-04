4f6264ac7f3eeeb08c78d0dfc6e2a4f77ae98d23
rkeeves <aprettydecentaddress@gmail.com>
2021-05-04 15:14:37 +0200

    (feat): util class for number handling

---
+++ src/main/java/com/rkeeves/p4/javafx/Numbers.java
@@ -0,0 +1,12 @@
+package com.rkeeves.p4.javafx;
+
+public class Numbers {
+
+    private Numbers(){
+
+    }
+
+    public static String doubleToString(Double d) {
+        return Double.toString(d);
+    }
+}
