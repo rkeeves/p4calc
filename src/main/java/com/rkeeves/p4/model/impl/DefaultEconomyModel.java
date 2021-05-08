b707d465e20e812074d80ee157aa1b0d5e3c081c
rkeeves <aprettydecentaddress@gmail.com>
2021-05-08 21:08:30 +0200

    Refactored economy model service implementation

--- src/main/java/com/rkeeves/p4/model/impl/DefaultEconomyModel.java
+++ src/main/java/com/rkeeves/p4/model/impl/DefaultEconomyModel.java
@@ -1,12 +1,14 @@
 package com.rkeeves.p4.model.impl;
 
 import com.rkeeves.p4.dto.EconomyDTO;
-import com.rkeeves.p4.model.EconomyParametersModel;
+import com.rkeeves.p4.javafx.ExpressionSquareMatrix;
 import com.rkeeves.p4.model.EconomyModel;
+import com.rkeeves.p4.model.EconomyParametersModel;
+import com.rkeeves.p4.model.LowerTriangleMatrix;
 import com.rkeeves.p4.model.ProductModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import lombok.*;
-
-import java.util.List;
 
 @Data
 @Builder(access = AccessLevel.PRIVATE)
@@ -15,20 +17,40 @@ public class DefaultEconomyModel implements EconomyModel {
 
     private final EconomyParametersModel economyParametersModel;
 
+    private final DependencyMatrix dependencyMatrix;
+
     @Getter(AccessLevel.PRIVATE)
-    private final List<ProductModel> productModels;
+    private final ObservableList<ProductModel> productModels;
 
     @Override
-    public List<ProductModel> listProductModels() {
+    public int getProductCount() {
+        return productModels.size();
+    }
+
+    @Override
+    public ObservableList<ProductModel> listProductModels() {
         return productModels;
+    }
+
+    @Override
+    public LowerTriangleMatrix getDependencyMatrixMutableLowerTriangle() {
+        return dependencyMatrix;
+    }
+
+    @Override
+    public ExpressionSquareMatrix getDependencyMatrix() {
+        return dependencyMatrix;
     }
 
     public static DefaultEconomyModel create(EconomyDTO dto){
         var economyConstantsModel = DefaultEconomyParametersModel.create(dto.getEconomyParameters());
-        var products = new ProductModelsFactory().create(economyConstantsModel, dto.getProducts());
+        var result = new ProductModelsFactory().create(economyConstantsModel, dto.getProducts());
+        var dependencyMatrix = result.getSome();
+        var productList = result.getOther();
         return DefaultEconomyModel.builder()
                 .economyParametersModel(economyConstantsModel)
-                .productModels(products)
+                .dependencyMatrix(dependencyMatrix)
+                .productModels(FXCollections.observableArrayList(productList))
                 .build();
     }
 }
