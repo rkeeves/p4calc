d92870e8cf2a9a529a1e32a592fbabaac52735d2
rkeeves <aprettydecentaddress@gmail.com>
2021-05-08 20:59:56 +0200

    Added specific exception for fxml

---
+++ src/main/java/com/rkeeves/p4/io/FXMLLoadFailedException.java
@@ -0,0 +1,8 @@
+package com.rkeeves.p4.io;
+
+public class FXMLLoadFailedException extends Exception{
+
+    public FXMLLoadFailedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
