0417d2da8fb76ed99a8b49db5b3a7deb820f6a83
rkeeves <aprettydecentaddress@gmail.com>
2021-05-09 15:37:29 +0200

    Added javadoc

--- src/main/java/com/rkeeves/p4/io/FXMLLoadFailedException.java
+++ src/main/java/com/rkeeves/p4/io/FXMLLoadFailedException.java
@@ -1,7 +1,16 @@
 package com.rkeeves.p4.io;
 
+/**
+ * An Exception to signal any errors during FXML loading operations.
+ */
 public class FXMLLoadFailedException extends Exception{
 
+    /**
+     * Constructs an instance based on user supplied message and cause.
+     * 
+     * @param message - user supplied error message
+     * @param cause - the underlying exception which was thrown by FXMLLoader
+     */
     public FXMLLoadFailedException(String message, Throwable cause) {
         super(message, cause);
     }
