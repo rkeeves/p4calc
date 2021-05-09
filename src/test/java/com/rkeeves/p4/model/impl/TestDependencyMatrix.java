fc51f9eccc9d203a35501ec921462b97f35d11ec
rkeeves <aprettydecentaddress@gmail.com>
2021-05-09 15:20:57 +0200

    Renamed test class of DependencyMatrix

--- src/test/java/com/rkeeves/p4/model/impl/TestDependencyMatrix.java
+++ src/test/java/com/rkeeves/p4/model/impl/TestDependencyMatrix.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-class DependencyMatrixTest {
+class TestDependencyMatrix {
 
     @Test
     void dependencyMatrix_WhenGivenDependency_CreatesValidMatrix(){
