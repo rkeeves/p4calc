f0cd768eddad10ba220c81f1efc54655a9accd29
rkeeves <aprettydecentaddress@gmail.com>
2021-05-09 15:29:03 +0200

    Added javadoc

--- src/main/java/com/rkeeves/p4/Entry.java
+++ src/main/java/com/rkeeves/p4/Entry.java
@@ -2,8 +2,16 @@ package com.rkeeves.p4;
 
 import javafx.application.Application;
 
+/**
+ * This entry provides the entry point to the program.
+ */
 public class Entry {
 
+    /**
+     * The program's entry point.
+     * 
+     * @param args - user given arguments passed to the program
+     */
     public static void main(String[] args) {
         Application.launch(CalculatorApplication.class, args);
     }
