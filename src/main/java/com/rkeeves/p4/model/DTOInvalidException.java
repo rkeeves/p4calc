789311d67699b30e3c7a00f413d97ee7f64acdd8
rkeeves <aprettydecentaddress@gmail.com>
2021-05-09 16:14:06 +0200

    Added javadoc

--- src/main/java/com/rkeeves/p4/model/DTOInvalidException.java
+++ src/main/java/com/rkeeves/p4/model/DTOInvalidException.java
@@ -1,7 +1,14 @@
 package com.rkeeves.p4.model;
 
+/**
+ * This Exception represents the invalidity of Data Transfer Objects.
+ */
 public class DTOInvalidException extends Exception{
 
+    /**
+     * Constructs and instance based on the user supplied error message.
+     * @param message error message
+     */
     public DTOInvalidException(String message) {
         super(message);
     }
