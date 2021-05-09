9ee95cd1bec7a74543e2065c7f2d94102e71a6f4
rkeeves <aprettydecentaddress@gmail.com>
2021-05-09 15:54:43 +0200

    Added javadoc

--- src/main/java/com/rkeeves/p4/io/JSONWriteFailedException.java
+++ src/main/java/com/rkeeves/p4/io/JSONWriteFailedException.java
@@ -1,7 +1,16 @@
 package com.rkeeves.p4.io;
 
+/**
+ * An Exception to signal JSON file write errors
+ */
 public class JSONWriteFailedException extends Exception{
 
+    /**
+     * Constructs an instance with the user supplied error message,
+     * and the
+     * @param message - user supplied error message
+     * @param cause - user supplied Throwable which caused this Exception
+     */
     public JSONWriteFailedException(String message, Throwable cause) {
         super(message, cause);
     }
