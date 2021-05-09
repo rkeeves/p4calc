1e87e38ba59fd769b8476eed6d6bb3365953b435
rkeeves <aprettydecentaddress@gmail.com>
2021-05-09 16:48:54 +0200

    Added javadoc

--- src/main/java/com/rkeeves/p4/model/ProductIngredientsModel.java
+++ src/main/java/com/rkeeves/p4/model/ProductIngredientsModel.java
@@ -5,11 +5,37 @@ import javafx.beans.property.DoubleProperty;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * This entity represents a product's ability to supply information about its ingredients.
+ */
 public interface ProductIngredientsModel {
 
+    /**
+     * Returns a map of all products which can be used as ingredients.
+     * Keys of the map are the products (via ProductBasicProperties interface).
+     * Values of the map are the actual required quantities.
+     * If a product is a possible ingredient, but it is not used for the recipe,
+     * the quantity described by the map entry's value will be zero.
+     *
+     * @return map of ingredient products and their current required quantity
+     */
     Map<ProductBasicPropertiesModel, DoubleProperty> getIngredients();
 
+    /**
+     * Returns optionally the current quantity for an ingredient.
+     * The optional is empty if and only if the product is not a possible ingredient.
+     *
+     * @param ingredient the ingredient to search for
+     * @return quantity of the ingredient or empty if the product is not a possible ingredient
+     */
     Optional<DoubleProperty> getIngredientQuantity(ProductBasicPropertiesModel ingredient);
 
+    /**
+     * Returns optionally the current quantity for an ingredient based on name.
+     * The optional is empty if and only if the product is not a possible ingredient.
+     *
+     * @param ingredientName the ingredient's name
+     * @return quantity of the ingredient or empty if the product is not a possible ingredient
+     */
     Optional<DoubleProperty> getIngredientQuantity(String ingredientName);
 }
