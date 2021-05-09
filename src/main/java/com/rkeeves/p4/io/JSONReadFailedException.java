5321179820b77f196eeb8c3715b805b033ca3af5
rkeeves <aprettydecentaddress@gmail.com>
2021-05-09 15:53:19 +0200

    Added javadoc

--- src/main/java/com/rkeeves/p4/io/JSONReadFailedException.java
+++ src/main/java/com/rkeeves/p4/io/JSONReadFailedException.java
@@ -1,11 +1,24 @@
 package com.rkeeves.p4.io;
 
+/**
+ * An Exception to signal JSON file read, parse and dto mapping errors
+ */
 public class JSONReadFailedException extends Exception{
 
+    /**
+     * Constructs an instance with the user supplied error message
+     * @param message - user supplied error message
+     */
     public JSONReadFailedException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs an instance with the user supplied error message,
+     * and the
+     * @param message - user supplied error message
+     * @param cause - user supplied Throwable which caused this Exception
+     */
     public JSONReadFailedException(String message, Throwable cause) {
         super(message, cause);
     }
