602cf1c46a64fa708cb105a7c44c52796dd7b571
rkeeves <aprettydecentaddress@gmail.com>
2021-05-09 15:28:39 +0200

    Added javadoc

--- src/main/java/com/rkeeves/p4/model/impl/DefaultEconomyModelService.java
+++ src/main/java/com/rkeeves/p4/model/impl/DefaultEconomyModelService.java
@@ -4,8 +4,18 @@ import com.rkeeves.p4.dto.EconomyDTO;
 import com.rkeeves.p4.model.EconomyModel;
 import com.rkeeves.p4.model.EconomyModelService;
 
+/**
+ * Default implementation of EconomyModel service
+ */
 public class DefaultEconomyModelService implements EconomyModelService {
 
+    /**
+     * This method constructs a JavaFX model object from the user supplied DTO.
+     * The model implements all business rules of the economy.
+     *
+     * @param dto - the Data Transfer Object representing the economy
+     * @return model implementng the rules of the economy
+     */
     public EconomyModel create(EconomyDTO dto){
         return DefaultEconomyModel.create(dto);
     }
