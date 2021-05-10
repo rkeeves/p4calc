8d276c2e0350df03f78d0f7c4fd566b60f818d83
rkeeves <aprettydecentaddress@gmail.com>
2021-05-10 11:36:56 +0200

    Added javadoc

--- src/main/java/com/rkeeves/p4/javafx/ExpressionSquareMatrix.java
+++ src/main/java/com/rkeeves/p4/javafx/ExpressionSquareMatrix.java
@@ -2,9 +2,24 @@ package com.rkeeves.p4.javafx;
 
 import javafx.beans.binding.NumberExpression;
 
+/**
+ * This entity represents a square matrix whose elements are NumberExpressions.
+ */
 public interface ExpressionSquareMatrix {
 
+    /**
+     * Returns the size of the square matrix.
+     *
+     * @return size of the square matrix
+     */
     int size();
 
+    /**
+     * Returns the element from at the user given row and column.
+     *
+     * @param row row of the selected element
+     * @param col column of the selected element
+     * @return expression at the user given coordinates
+     */
     NumberExpression get(int row, int col);
 }
