53b41a10f706b1745ad10b8eb6bf9c9872109e3e
rkeeves <aprettydecentaddress@gmail.com>
2021-05-10 11:36:13 +0200

    Added javadoc

--- src/main/java/com/rkeeves/p4/javafx/ExpressionVector.java
+++ src/main/java/com/rkeeves/p4/javafx/ExpressionVector.java
@@ -2,9 +2,24 @@ package com.rkeeves.p4.javafx;
 
 import javafx.beans.binding.NumberExpression;
 
+
+/**
+ * Represents an expression vector whose elements can be read.
+ */
 public interface ExpressionVector {
 
+    /**
+     * Returns the size of the expression vector.
+     *
+     * @return size of the expression vector
+     */
     int size();
 
+    /**
+     * Returns the expression at the user given index.
+     *
+     * @param index the index of the queried element
+     * @return expression at the user given index
+     */
     NumberExpression get(int index);
 }
