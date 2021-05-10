37a10d4a067411959179da907a32dd58ed40a32f
rkeeves <aprettydecentaddress@gmail.com>
2021-05-10 11:36:36 +0200

    Added javadoc.

--- src/main/java/com/rkeeves/p4/javafx/MutableExpressionVector.java
+++ src/main/java/com/rkeeves/p4/javafx/MutableExpressionVector.java
@@ -2,7 +2,17 @@ package com.rkeeves.p4.javafx;
 
 import javafx.beans.binding.NumberExpression;
 
+/**
+ * Represents an expression vector whose elements can be changed.
+ */
 public interface MutableExpressionVector extends ExpressionVector{
 
+    /**
+     * Swaps the old number expression of the vector at the given index with the
+     * new number expression given by the user.
+     *
+     * @param index the index of the element
+     * @param expr the new element
+     */
     void set(int index, NumberExpression expr);
 }
