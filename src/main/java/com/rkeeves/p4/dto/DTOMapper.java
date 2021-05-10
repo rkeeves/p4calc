bde3d7c4fdc450dcf83311c25e7da400c4b285d4
rkeeves <aprettydecentaddress@gmail.com>
2021-05-10 08:51:34 +0200

    Added javadoc

--- src/main/java/com/rkeeves/p4/dto/DTOMapper.java
+++ src/main/java/com/rkeeves/p4/dto/DTOMapper.java
@@ -1,6 +1,17 @@
 package com.rkeeves.p4.dto;
 
+/**
+ * This entity represents an object capable of transforming a model object into a Data Transfer Object.
+ * @param <T> Model type
+ * @param <D> Data Transfer Object type
+ */
 public interface DTOMapper<T,D> {
 
+    /**
+     * Given a model object transforms it into a Data Transfer Object.
+     *
+     * @param object model object instance
+     * @return instance of the Data Transfer Object
+     */
     D toDTO(T object);
 }
