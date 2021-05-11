9cdeceef2cf516fdba61459884fc86fced054775
rkeeves <aprettydecentaddress@gmail.com>
2021-05-11 17:19:25 +0200

    Added default values

--- src/test/java/com/rkeeves/p4/dtomap/MockEconomyParametersModel.java
+++ src/test/java/com/rkeeves/p4/dtomap/MockEconomyParametersModel.java
@@ -8,21 +8,21 @@ import javafx.beans.property.SimpleIntegerProperty;
 import lombok.Data;
 
 @Data
-class MockEconomyParametersModel implements EconomyParametersModel {
+public class MockEconomyParametersModel implements EconomyParametersModel {
 
-    private final DoubleProperty kgPerBarrelProperty = new SimpleDoubleProperty();
+    private final DoubleProperty kgPerBarrelProperty = new SimpleDoubleProperty(1900);
 
-    private final DoubleProperty globalMarketDemandMultiplierProperty = new SimpleDoubleProperty();
+    private final DoubleProperty globalMarketDemandMultiplierProperty = new SimpleDoubleProperty(1.0);
 
-    private final IntegerProperty consumerCountProperty = new SimpleIntegerProperty();
+    private final IntegerProperty consumerCountProperty = new SimpleIntegerProperty(10000);
 
-    private final DoubleProperty globalSellPriceMultiplierProperty = new SimpleDoubleProperty();
+    private final DoubleProperty globalSellPriceMultiplierProperty = new SimpleDoubleProperty(1.75);
 
-    private final IntegerProperty workersPerWorkshopProperty = new SimpleIntegerProperty();
+    private final IntegerProperty workersPerWorkshopProperty = new SimpleIntegerProperty(25);
 
-    private final IntegerProperty wagePerWorkerProperty = new SimpleIntegerProperty();
+    private final IntegerProperty wagePerWorkerProperty = new SimpleIntegerProperty(6);
 
-    private final IntegerProperty fixedPropertyTaxProperty = new SimpleIntegerProperty();
+    private final IntegerProperty fixedPropertyTaxProperty = new SimpleIntegerProperty(40);
 
-    private final IntegerProperty fixedRunningCostProperty = new SimpleIntegerProperty();
+    private final IntegerProperty fixedRunningCostProperty = new SimpleIntegerProperty(50);
 }
