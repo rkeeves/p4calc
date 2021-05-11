410b8dbe7950182d1c1f6d5411c21a898b3ea61d
rkeeves <aprettydecentaddress@gmail.com>
2021-05-11 19:49:29 +0200

    Splitted new class from ProductModelsFactory

---
+++ src/main/java/com/rkeeves/p4/model/impl/IngredientPopulator.java
@@ -0,0 +1,36 @@
+package com.rkeeves.p4.model.impl;
+
+import com.rkeeves.p4.javafx.ExpressionVector;
+import com.rkeeves.p4.model.ProductModel;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+class IngredientPopulator {
+
+    static List<ProductModel> create(DependencyMatrix dependencyMatrix,
+                                                     ExpressionVector sumDemands,
+                                                     List<DefaultProductModel> productModels){
+        bindSumDemandsToProductModels(productModels, sumDemands);
+        populateIngredientMaps(productModels, dependencyMatrix);
+        return productModels.stream()
+                .map(ProductModel.class::cast)
+                .collect(Collectors.toList());
+    }
+
+    static void bindSumDemandsToProductModels(List<DefaultProductModel> productModels, ExpressionVector sumDemands) {
+        for (int i = 0; i < productModels.size(); i++) {
+            productModels.get(i).bindSumDemandExpression(sumDemands.get(i));
+        }
+    }
+
+    static void populateIngredientMaps(List<DefaultProductModel> productModels, DependencyMatrix dependencyMatrix) {
+        for (int productIndex = 0; productIndex < productModels.size(); productIndex++) {
+            var product = productModels.get(productIndex);
+            var ingredients = product.getIngredients();
+            for (int ingredientIndex = 0; ingredientIndex < productIndex; ingredientIndex++) {
+                ingredients.put(productModels.get(ingredientIndex),dependencyMatrix.getElementOfLowerTriangle(productIndex,ingredientIndex));
+            }
+        }
+    }
+}
