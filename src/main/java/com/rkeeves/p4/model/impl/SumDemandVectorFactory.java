86fb93841a662fea8ab40c1fe1c92cf7867ed449
rkeeves <aprettydecentaddress@gmail.com>
2021-05-11 19:49:44 +0200

    Splitted new class from ProductModelsFactory

---
+++ src/main/java/com/rkeeves/p4/model/impl/SumDemandVectorFactory.java
@@ -0,0 +1,27 @@
+package com.rkeeves.p4.model.impl;
+
+import com.rkeeves.p4.javafx.ExpressionVector;
+import com.rkeeves.p4.javafx.ExpressionVectorAdapter;
+import javafx.beans.binding.NumberExpression;
+
+import java.util.List;
+
+import static com.rkeeves.p4.javafx.ExpressionSquareMatrices.multiplyMatrixWithVector;
+
+class SumDemandVectorFactory {
+
+    static ExpressionVector createSumDemandVector(DependencyMatrix dependencyMatrix, List<DefaultProductModel> productModels){
+        var demandMatrix = new DemandMatrix(dependencyMatrix);
+        var marketDemandsVector = createMarketDemandVector(productModels);
+        return multiplyMatrixWithVector(demandMatrix, marketDemandsVector);
+    }
+
+
+    static ExpressionVector createMarketDemandVector(List<DefaultProductModel> productModels){
+        var marketDemandsArray = new NumberExpression[productModels.size()];
+        for (int i = 0; i < productModels.size(); i++) {
+            marketDemandsArray[i] = productModels.get(i).getMarketDemandProperty();
+        }
+        return new ExpressionVectorAdapter<>(marketDemandsArray);
+    }
+}
