4dfb203ad4e29151be464973bb95981a08d244c0
rkeeves <aprettydecentaddress@gmail.com>
2021-05-11 19:50:04 +0200

    Removed reliance on ProductModelsFactory

--- src/main/java/com/rkeeves/p4/model/impl/DefaultEconomyModel.java
+++ src/main/java/com/rkeeves/p4/model/impl/DefaultEconomyModel.java
@@ -1,6 +1,7 @@
 package com.rkeeves.p4.model.impl;
 
 import com.rkeeves.p4.dto.EconomyDTO;
+import com.rkeeves.p4.dto.ProductDTO;
 import com.rkeeves.p4.model.EconomyModel;
 import com.rkeeves.p4.model.EconomyParametersModel;
 import com.rkeeves.p4.model.LowerTriangleMatrix;
@@ -8,6 +9,9 @@ import com.rkeeves.p4.model.ProductModel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import lombok.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @Builder(access = AccessLevel.PRIVATE)
@@ -33,13 +37,21 @@ class DefaultEconomyModel implements EconomyModel {
 
     static DefaultEconomyModel create(EconomyDTO dto){
         var economyConstantsModel = DefaultEconomyParametersModel.create(dto.getEconomyParameters());
-        var result = new ProductModelsFactory().create(economyConstantsModel, dto.getProducts());
-        var dependencyMatrix = result.getKey();
-        var productList = result.getValue();
+        var defaultProductModels = createProductModels(economyConstantsModel, dto.getProducts());
+        var dependencyMatrix = DependencyMatrixFactory.createDependencyMatrix(defaultProductModels);
+        var sumDemandVector = SumDemandVectorFactory.createSumDemandVector(dependencyMatrix,defaultProductModels);
+        IngredientPopulator.create(dependencyMatrix,sumDemandVector,defaultProductModels);
         return DefaultEconomyModel.builder()
                 .economyParametersModel(economyConstantsModel)
                 .dependencyMatrix(dependencyMatrix)
-                .productModels(FXCollections.observableArrayList(productList))
+                .productModels(FXCollections.observableArrayList(defaultProductModels))
                 .build();
+    }
+
+    private static List<DefaultProductModel> createProductModels(EconomyParametersModel constants, List<ProductDTO> productDTOs){
+        return productDTOs.stream()
+                .sequential()
+                .map(productDTO->DefaultProductModel.create(constants, productDTO))
+                .collect(Collectors.toList());
     }
 }
