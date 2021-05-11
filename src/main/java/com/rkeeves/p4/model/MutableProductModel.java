5f2df37f5d34198c9e2e393c19cefa18959ed81f
rkeeves <aprettydecentaddress@gmail.com>
2021-05-11 21:36:30 +0200

    Added interface for loose coupling

---
+++ src/main/java/com/rkeeves/p4/model/MutableProductModel.java
@@ -0,0 +1,7 @@
+package com.rkeeves.p4.model;
+
+/**
+ * This entity represents a still mutable Product Model.
+ */
+public interface MutableProductModel extends ProductModel, MutableProductDemandModel {
+}
