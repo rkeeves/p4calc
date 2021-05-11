f77f2e7c7e4371c67d2ec68fc671aba6bf9fe2f3
rkeeves <aprettydecentaddress@gmail.com>
2021-05-11 21:39:52 +0200

    Reduced class visibility

--- src/test/java/com/rkeeves/p4/model/impl/TestSumDemandVectorFactory.java
+++ src/test/java/com/rkeeves/p4/model/impl/TestSumDemandVectorFactory.java
@@ -16,7 +16,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestSumDemandVectorFactory {
+class TestSumDemandVectorFactory {
 
     @RequiredArgsConstructor
     static class SimpleExpressionSquareMatrix implements ExpressionSquareMatrix {
