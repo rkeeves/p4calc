24e5c5f264240b02d1175c8b5f36ef851aac6f8d
rkeeves <aprettydecentaddress@gmail.com>
2021-05-13 14:47:15 +0200

    Changed javadoc

--- src/main/java/com/rkeeves/p4/dtomap/EconomyDTOService.java
+++ src/main/java/com/rkeeves/p4/dtomap/EconomyDTOService.java
@@ -4,15 +4,15 @@ import com.rkeeves.p4.dto.EconomyDTO;
 import com.rkeeves.p4.model.EconomyModel;
 
 /**
- * This service interface represents an entity which can transform a valid model into a Data Transfer Object.
+ * This service interface represents an entity which can transform a valid model into a {@code Data Transfer Object}.
  */
 public interface EconomyDTOService {
 
     /**
-     * This method transforms a user given valid economy model into a valid dto.
+     * This method transforms a user given valid economy model into a valid {@code Data Transfer Object}.
      *
      * @param economyModel user given economy model object
-     * @return data transfer object of economy
+     * @return {@code Data Transfer Object} of economy
      */
     EconomyDTO toDto(EconomyModel economyModel);
 }
