02a5b4cf5ac62890e694e2b2408336c199a0f451
rkeeves <aprettydecentaddress@gmail.com>
2021-05-13 14:59:57 +0200

    Changed javadoc

--- src/main/java/com/rkeeves/p4/model/EconomyModelService.java
+++ src/main/java/com/rkeeves/p4/model/EconomyModelService.java
@@ -3,14 +3,14 @@ package com.rkeeves.p4.model;
 import com.rkeeves.p4.dto.EconomyDTO;
 
 /**
- * This service is responsible for providing EconomyModel for the user.
+ * This service is responsible for providing {@code EconomyModel} for the user.
  */
 public interface EconomyModelService {
 
     /**
-     * Based on the economy model dto given by a user, provides a Property-based model of the economy.
+     * Based on the economy model dto given by a user, provides a {@code Property}-based model of the economy.
      *
-     * @param dto the Data Transfer Object representing the economy
+     * @param dto the {@code Data Transfer Object} representing the economy
      * @return model of the economy
      */
     EconomyModel create(EconomyDTO dto) throws DTOInvalidException;
