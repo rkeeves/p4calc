3e56aa23967b219c4301596152bf1d7205da1d1d
rkeeves <aprettydecentaddress@gmail.com>
2021-05-13 13:22:24 +0200

    Changed javadoc

--- src/main/lombok/com/rkeeves/p4/dto/ProductDTO.java
+++ src/main/lombok/com/rkeeves/p4/dto/ProductDTO.java
@@ -13,11 +13,11 @@ public class ProductDTO {
 
     /**
      * -- GETTER --
-     * Returns the name of the product.
+     * Returns the name of the {@code ProductDTO}.
      *
-     * @return the product's name
+     * @return the {@code ProductDTO}'s name
      * -- SETTER --
-     * Changes the name of the product.
+     * Changes the name of the {@code ProductDTO}.
      *
      * @param name new name
      */
@@ -25,14 +25,14 @@ public class ProductDTO {
 
     /**
      * -- GETTER --
-     * Returns the map of ingredients used by this product.
-     * Keys are the ingredient products' names, while values are the needed amount.
+     * Returns the map of ingredients used by this {@code ProductDTO}.
+     * Keys are the ingredient {@code ProductDTO}' names, while values are the needed amount.
      *
      * @return ingredients map
      * -- SETTER --
-     * Changes the map of ingredients used by this product.
+     * Changes the map of ingredients used by this {@code ProductDTO}.
      *
-     * @param ingredients new ingredients map
+     * @param ingredients new map of ingredient names and ingredient quantities
      */
     private Map<String, Double> ingredients = new HashMap<>();
 
@@ -53,11 +53,11 @@ public class ProductDTO {
 
     /**
      * -- GETTER --
-     * Returns the base daily demand per capita for this product.
+     * Returns the base daily demand per capita for this {@code ProductDTO}.
      *
      * @return base demand in kg
      * -- SETTER --
-     * Changes the base daily demand per capita for this product.
+     * Changes the base daily demand per capita for this {@code ProductDTO}.
      *
      * @param baseDemandInKg new base demand in kg
      */
@@ -65,11 +65,11 @@ public class ProductDTO {
 
     /**
      * -- GETTER --
-     * Returns the base daily output in barrels for a given workshop which produces this product.
+     * Returns the base daily output in barrels for a given workshop which produces this {@code ProductDTO}.
      *
      * @return production per workshop in barrels
      * -- SETTER --
-     * Changes the base daily output in barrels for a given workshop which produces this product.
+     * Changes the base daily output in barrels for a given workshop which produces this {@code ProductDTO}.
      *
      * @param productionPerWorkshop new production per workshop in barrels
      */
@@ -77,11 +77,11 @@ public class ProductDTO {
 
     /**
      * -- GETTER --
-     * Returns the base price this product.
+     * Returns the base price this {@code ProductDTO}.
      *
      * @return base price
      * -- SETTER --
-     * Changes the base price this product.
+     * Changes the base price this {@code ProductDTO}.
      *
      * @param basePrice new base price
      */
@@ -89,12 +89,12 @@ public class ProductDTO {
 
 
     /**
-     * Returns how many ingredients (in barrel) are used for the particular product,
+     * Returns how many ingredients (in barrel) are used for the particular {@code ProductDTO},
      * which has the user given name.
-     * If the product by the user given name is not an actual ingredient it returns zero.
+     * If the {@code ProductDTO} by the user given name is not an actual ingredient it returns zero.
      *
      * @param ingredient
-     * @return the ingredient's used amount, or zero if the ingredient is unused for this product
+     * @return the ingredient's used amount, or zero if the ingredient is unused for this {@code ProductDTO}
      */
     public Double getIngredientWeightOrZero(String ingredient){
         return ingredients.getOrDefault(ingredient,0.0);
