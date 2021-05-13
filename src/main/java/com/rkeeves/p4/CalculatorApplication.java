c5b88cd07f932b19dd838a859a2eebe0c29274b5
rkeeves <aprettydecentaddress@gmail.com>
2021-05-13 22:31:50 +0200

    Application now uses StageController class

--- src/main/java/com/rkeeves/p4/CalculatorApplication.java
+++ src/main/java/com/rkeeves/p4/CalculatorApplication.java
@@ -1,49 +1,84 @@
 package com.rkeeves.p4;
 
 import com.rkeeves.p4.controller.*;
+import com.rkeeves.p4.dto.EconomyDTO;
 import com.rkeeves.p4.dtomap.DefaultEconomyDTOService;
-import com.rkeeves.p4.dtomap.EconomyDTOService;
 import com.rkeeves.p4.io.*;
-import com.rkeeves.p4.model.EconomyModelService;
-import com.rkeeves.p4.model.impl.DefaultEconomyModelService;
+import com.rkeeves.p4.model.impl.DefaultEconomyModelCreatorService;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.tinylog.Logger;
+
+import java.util.List;
 
 /**
  * This entity is the {@code JavaFX} {@code Application} itself.
  */
 public class CalculatorApplication extends Application {
 
-    private JSONService jsonService;
+    private static final String MAIN_VIEW_FXML = "/fxml/mainView.fxml";
+
+    private static final String DEFAULT_ECONOMY_DATA_JSON_RESOURCE_NAME = "/data/defaultEconomyData.json";
+
+    private static final String BASE_CSS = resourceExternalForm("/css/base.css");
+
+    private static final String LIGHT_CSS = resourceExternalForm("/css/light-theme.css");
+
+    private static String resourceExternalForm(String resourceName){
+        return DefaultStyleSheetService.class.getResource(resourceName).toExternalForm();
+    }
 
     private FXMLService fxmlService;
 
-    private EconomyModelService economyModelService;
-
-    private EconomyDTOService economyDTOService;
-
     private StyleSheetService styleSheetService;
 
-    private AlertController alertController;
+    private NewAction newAction;
 
-    private FileChooserController fileChooserController;
+    private OpenAction openAction;
 
+    private SaveAsAction saveAsAction;
+
+    private QuitAction quitAction;
+
+    private ChangeStyleSheetAction defaultThemeAction;
+
+    private ChangeStyleSheetAction lightThemeAction;
+
+    private EconomyModelService economyModelService;
+
+    private StageController stageController;
     /**
      * This method gets called after an {@code CalculatorApplication} instance is constructed.
      * This serves as a way to setup dependencies.
      */
     @Override
     public void init() {
-        jsonService = new JacksonJSONService();
+        Logger.debug("Application initialization started");
+        var jsonService = new JacksonJSONService();
+        EconomyDTO defaultEconomyDTO;
+        try {
+            defaultEconomyDTO = jsonService.readFromResource(DEFAULT_ECONOMY_DATA_JSON_RESOURCE_NAME, EconomyDTO.class);
+        } catch (JSONReadFailedException e) {
+            Logger.error("Loading default economy json data failed.",e);
+            Platform.exit();
+            return;
+        }
         fxmlService = new DefaultFXMLService();
-        economyModelService = new DefaultEconomyModelService();
-        economyDTOService = new DefaultEconomyDTOService();
         styleSheetService = new DefaultStyleSheetService();
-        alertController = new AlertController(styleSheetService);
-        fileChooserController = new FileChooserController();
+        var economyModelService = new DefaultEconomyModelCreatorService();
+        var economyDTOService = new DefaultEconomyDTOService();
+        var alertController = new AlertController(styleSheetService);
+        var fileChooserController = new FileChooserController();
+        this.economyModelService = new DefaultEconomyModelService();
+        newAction = new NewAction(defaultEconomyDTO, economyModelService, this.economyModelService, alertController);
+        openAction = new OpenAction(fileChooserController,jsonService,economyModelService, this.economyModelService, alertController);
+        saveAsAction = new SaveAsAction(this.economyModelService,jsonService,economyDTOService,fileChooserController,alertController);
+        quitAction = new QuitAction(alertController);
+        defaultThemeAction = new ChangeStyleSheetAction(styleSheetService, List.of(BASE_CSS));
+        lightThemeAction = new ChangeStyleSheetAction(styleSheetService, List.of(BASE_CSS, LIGHT_CSS));
         Logger.debug("Application initialized");
     }
 
@@ -56,15 +91,26 @@ public class CalculatorApplication extends Application {
     public void start(Stage stage) {
         try {
             Logger.debug("Application started");
+            var mainController = new DefaultMainController();
+            VBox vBox = fxmlService.load(mainController,MAIN_VIEW_FXML);
+            var scene = new Scene(vBox);
             stage.setTitle("Calculator");
             stage.setMaximized(true);
-            VBox vBox = fxmlService.load(new MainController(economyModelService,economyDTOService,jsonService,fxmlService,styleSheetService,alertController,fileChooserController),
-                    "/fxml/mainView.fxml");
-
-            stage.setScene(new Scene(vBox));
-            var scene = stage.getScene();
-            styleSheetService.changeThemeToLight();
-            scene.getStylesheets().addAll(styleSheetService.getStyleSheets());
+            stage.setScene(scene);
+            stageController = StageController.builder()
+                    .mainController(mainController)
+                    .stage(stage)
+                    .economyModelService(economyModelService)
+                    .fxmlService(fxmlService)
+                    .styleSheetService(styleSheetService)
+                    .newAction(newAction)
+                    .openAction(openAction)
+                    .saveAsAction(saveAsAction)
+                    .quitAction(quitAction)
+                    .defaultThemeAction(defaultThemeAction)
+                    .lightThemeAction(lightThemeAction)
+                    .baseCSS(BASE_CSS)
+                    .build();
             stage.show();
         } catch (FXMLLoadFailedException e) {
             var runtimeExc = new RuntimeException("Wasn't able to load main view",e);
@@ -72,7 +118,6 @@ public class CalculatorApplication extends Application {
             throw runtimeExc;
         }
     }
-
     /**
      * Called after {@code Platform.exit()} was called explicitly or all managed windows were closed.
      * This method was overridden only to facilitate logging.
