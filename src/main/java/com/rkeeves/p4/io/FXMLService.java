f79afc16110780a91c5cf4cfcd68f2ebde380c6a
rkeeves <aprettydecentaddress@gmail.com>
2021-05-13 14:38:49 +0200

    Changed javadoc

--- src/main/java/com/rkeeves/p4/io/FXMLService.java
+++ src/main/java/com/rkeeves/p4/io/FXMLService.java
@@ -3,15 +3,15 @@ package com.rkeeves.p4.io;
 import javafx.scene.Node;
 
 /**
- * This service provides a generic way for loading fxml based views with user supplied controllers.
+ * This service provides a generic way for loading {@code FXML} based views with user supplied controllers.
  */
 public interface FXMLService {
 
     /**
-     * Loads the fxml based view from the location given by the user.
-     * The user supplied controller instance will be used as controller for FXMLLoader.
-     * This method does NOT validate whether all FXML annotated fields of the controller
-     * were injected by the FXMLLoader, so use this with caution.
+     * Loads the {@code FXML} based view from the location given by the user.
+     * The user supplied controller instance will be used as controller for {@code FXMLLoader}.
+     * This method does NOT validate whether all {@code FXML} annotated fields of the controller
+     * were injected by the {@code FXMLLoader}, so use this with caution.
      *
      * @param controller the controller instance
      * @param fxmlResourcePath fxml file's resource name string
