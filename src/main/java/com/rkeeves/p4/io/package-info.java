d883c3bbb81bf1e221a86ebcdbece714594a908a
rkeeves <aprettydecentaddress@gmail.com>
2021-05-13 14:40:25 +0200

    Changed javadoc

--- src/main/java/com/rkeeves/p4/io/package-info.java
+++ src/main/java/com/rkeeves/p4/io/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Provides classes to deal with read/write of JSON and FXML files.
+ * Provides classes to deal with read/write of {@code JSON} and {@code FXML} files.
  */
 package com.rkeeves.p4.io;
