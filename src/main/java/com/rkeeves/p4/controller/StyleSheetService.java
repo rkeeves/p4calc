740a9279a0cadb69e5d61500d1a917ea1ad5364c
rkeeves <aprettydecentaddress@gmail.com>
2021-05-13 22:24:59 +0200

    Service now handles listeners

--- src/main/java/com/rkeeves/p4/controller/StyleSheetService.java
+++ src/main/java/com/rkeeves/p4/controller/StyleSheetService.java
@@ -2,11 +2,13 @@ package com.rkeeves.p4.controller;
 
 import javafx.collections.ObservableList;
 
+import java.util.List;
+
 public interface StyleSheetService {
 
     ObservableList<String> getStyleSheets();
 
-    void changeThemeToDefault();
+    void addListener(StyleSheetsChangeListener listener);
 
-    void changeThemeToLight();
+    void setStyleSheets(List<String> styleSheets);
 }
