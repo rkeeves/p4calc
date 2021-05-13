ef6de18eece2c740a3986e2aa38ddb239c23cc37
rkeeves <aprettydecentaddress@gmail.com>
2021-05-13 15:01:35 +0200

    Changed javadoc

--- src/main/java/com/rkeeves/p4/model/MutableProductModel.java
+++ src/main/java/com/rkeeves/p4/model/MutableProductModel.java
@@ -1,7 +1,7 @@
 package com.rkeeves.p4.model;
 
 /**
- * This entity represents a still mutable Product Model.
+ * This entity represents a still mutable (constructed but not yet fully finished) {@code ProductModel}.
  */
 public interface MutableProductModel extends ProductModel, MutableProductDemandModel {
 }
