faa338ad2afc77633e57f7b8d17a13b84432d816
rkeeves <aprettydecentaddress@gmail.com>
2021-05-13 22:29:12 +0200

    Objectified command

---
+++ src/main/java/com/rkeeves/p4/controller/NewAction.java
@@ -0,0 +1,52 @@
+package com.rkeeves.p4.controller;
+
+import com.rkeeves.p4.dto.EconomyDTO;
+import com.rkeeves.p4.io.FXMLLoadFailedException;
+import com.rkeeves.p4.model.DTOInvalidException;
+import com.rkeeves.p4.model.EconomyModelCreatorService;
+import lombok.RequiredArgsConstructor;
+import org.tinylog.Logger;
+
+@RequiredArgsConstructor
+public class NewAction implements Runnable{
+
+    private final EconomyDTO defaultEconomyDTO;
+
+    private final EconomyModelCreatorService economyModelCreatorService;
+
+    private final EconomyModelService economyModelService;
+
+    private final AlertController alertController;
+
+    @Override
+    public void run() {
+        try {
+            performNew();
+        }catch (DTOInvalidException e){
+            onDTOTransformToModelFailed(e);
+        } catch (FXMLLoadFailedException e) {
+            onModelResetFailed(e);
+        }
+    }
+
+    private void performNew() throws DTOInvalidException, FXMLLoadFailedException {
+        var newEconomyModel = economyModelCreatorService.create(defaultEconomyDTO);
+        economyModelService.setEconomyModel(newEconomyModel);
+        onModelResetSuccess();
+    }
+
+    private void onDTOTransformToModelFailed(DTOInvalidException e) {
+        Logger.error(e,"Transforming DTO to model failed");
+        alertController.showOkOnlyAlert("JSON Semantic Error","The json data contained semantic errors");
+    }
+
+    private void onModelResetSuccess() {
+        Logger.debug("Model reset succeeded");
+        alertController.showOkOnlyAlert("New model","Loading new (default) model succeeded");
+    }
+
+    private void onModelResetFailed(FXMLLoadFailedException e) {
+        Logger.error(e, "Model reset failed");
+        alertController.showOkOnlyAlert("Model Error","Wasn't able to set up model");
+    }
+}
