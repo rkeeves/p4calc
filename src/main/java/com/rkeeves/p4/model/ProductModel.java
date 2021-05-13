1eb36b3f32ab9f31183407adec314ab5d3de829d
rkeeves <aprettydecentaddress@gmail.com>
2021-05-13 15:03:31 +0200

    Changed javadoc

--- src/main/java/com/rkeeves/p4/model/ProductModel.java
+++ src/main/java/com/rkeeves/p4/model/ProductModel.java
@@ -2,6 +2,7 @@ package com.rkeeves.p4.model;
 
 /**
  * This entity provides access to all properties of a given product.
+ * This interface is simply a collection of other more fine-grained interfaces.
  */
 public interface ProductModel extends ProductBasicPropertiesModel,
         ProductDemandModel,
