5bb1eee0e3be175a19d2d4afb3928dd6d36f5483
rkeeves <aprettydecentaddress@gmail.com>
2021-05-13 14:41:20 +0200

    Changed javadoc

--- src/main/java/com/rkeeves/p4/package-info.java
+++ src/main/java/com/rkeeves/p4/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Contains entry point and the JavaFX application itself.
- * Also contains all necessary parts of the application in subpackages.
+ * Contains entry point and the {@code JavaFX Application} itself.
+ * Also contains all necessary parts of the program in subpackages.
  */
 package com.rkeeves.p4;
