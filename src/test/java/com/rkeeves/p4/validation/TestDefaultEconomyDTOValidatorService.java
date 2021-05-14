76927f30ea2214d7d8adfd987516c65d52612396
rkeeves <aprettydecentaddress@gmail.com>
2021-05-14 14:34:04 +0200

    Decreased visibility of test class

--- src/test/java/com/rkeeves/p4/validation/TestDefaultEconomyDTOValidatorService.java
+++ src/test/java/com/rkeeves/p4/validation/TestDefaultEconomyDTOValidatorService.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class TestDefaultEconomyDTOValidatorService {
+class TestDefaultEconomyDTOValidatorService {
     private static JSONService jsonService;
 
     private static EconomyDTOValidatorService validatorService;
